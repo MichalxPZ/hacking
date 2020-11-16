5c6c84a3c208a61c9a55a541461fdecb77d85d64
Michal <michauz2001@gmail.com>
2020-11-16 11:49:39 +0100

    Delete asfs.py

--- asfs.py
+++
@@ -1,9 +0,0 @@
-import itertools
-import string
-
-def password_generator():
-    for password in itertools.combinations(string.ascii_letters + string.digits, 1):
-        yield password[0]
-
-for password in password_generator():
-    print(password)
